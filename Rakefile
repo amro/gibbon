d6bc954ab37a51f9e13077b1a87b9c59bfc9dfda
Amro Mousa <amromousa@gmail.com>
2010-11-13 20:21:28 -0500

    Initial checkin

--- Rakefile
+++ Rakefile
@@ -15,13 +15,13 @@ Jeweler::Tasks.new do |gem|
   gem.name = "gibbon"
   gem.homepage = "http://github.com/amro/gibbon"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
+  gem.summary = %Q{Gibbon is a simple API wrapper for interacting with MailChimp API 1.3}
+  gem.description = %Q{Gibbon is a simple API wrapper for interacting with MailChimp API version 1.3. It supports }
   gem.email = "amromousa@gmail.com"
   gem.authors = ["Amro Mousa"]
   # Include your dependencies below. Runtime dependencies are required when using your gem,
   # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
-  #  gem.add_runtime_dependency 'jabber4r', '> 0.1'
+   gem.add_runtime_dependency 'httparty', '> 0.6.1'
   #  gem.add_development_dependency 'rspec', '> 1.2.3'
 end
 Jeweler::RubygemsDotOrgTasks.new
---
+++ lib/gibbon.rb
@@ -0,0 +1,36 @@
+module Gibbon
+  class API
+    include HTTParty
+
+    def initialize(apikey, extra_params = {})
+      @apikey = apikey
+      @default_params = {:apikey => apikey}.merge(extra_params)
+    end
+
+    def base_api_url
+      "https://#{@apikey.split("-").last}.api.mailchimp.com/1.3/?method="
+    end
+
+    def call(method, params = {})
+      url = base_api_url + method
+      params = params.merge(@default_params)
+      response = API.post(url, :query => params.as_json)
+
+      begin
+        response = JSON(response.body)
+      rescue
+        response = response.body
+      end
+      response
+    end
+
+    def method_missing(method, *args)
+      method = method.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } #Thanks Rails!
+      method = method[0].chr.downcase + method[1..-1].gsub(/aim$/i, 'AIM')
+      args = {} unless args.length > 0
+      args = args[0] if (args.class.to_s == "Array")
+      call(method, args)
+    end
+
+  end
+end
