7ec2a5b51840d57634e3d2ed081a5eb673c48557
Dave Worth <dave@highgroove.com>
2011-10-27 16:05:32 -0400

    update gibbon to use first (per upstream)
    update README for my travis-ci

--- lib/gibbon.rb
+++ lib/gibbon.rb
@@ -93,12 +93,6 @@ protected
       raise "Error from MailChimp Export API: #{first_line_object["error"]} (code #{first_line_object["code"]})" if first_line_object.is_a?(Hash) && first_line_object["error"]
     end
 
-    lines = response.body.lines
-    if @throws_exceptions
-      first_line_object = ActiveSupport::JSON.decode(lines.peek) if lines.peek
-      raise "Error from MailChimp Export API: #{first_line_object["error"]} (code #{first_line_object["code"]})" if first_line_object.is_a?(Hash) && first_line_object["error"]
-    end
-
     lines
   end
 end
