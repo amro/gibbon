5302a720b909c09ac5974e2dedb9b27ee6e6ed1c
Kir Shatrov <shatrov@me.com>
2013-06-25 22:28:27 +0400

    Fixed set_instance_defaults

--- lib/gibbon/api.rb
+++ lib/gibbon/api.rb
@@ -1,7 +1,7 @@
 module Gibbon
   class API
     attr_accessor :api_key, :api_endpoint, :timeout, :throws_exceptions
-  
+
     def initialize(api_key = nil, default_parameters = {})
       @api_key = api_key || self.class.api_key || ENV['MAILCHIMP_API_KEY']
       @api_key = @api_key.strip if @api_key
@@ -9,16 +9,24 @@ module Gibbon
       @api_endpoint = default_parameters.delete(:api_endpoint)
       @timeout = default_parameters.delete(:timeout)
       @throws_exceptions = default_parameters.delete(:throws_exceptions)
-  
+
       @default_params = { apikey: @api_key }.merge(default_parameters)
 
       set_instance_defaults
     end
 
+    def set_instance_defaults
+      @timeout = (API.timeout || 30) if @timeout.nil?
+      @api_endpoint = API.api_endpoint if @api_endpoint.nil?
+      # Two lines because the class variable could be false and (false || true) is always true
+      @throws_exceptions = API.throws_exceptions if @throws_exceptions.nil?
+      @throws_exceptions = true if @throws_exceptions.nil?
+    end
+
     def get_exporter
       Export.new(@api_key, @default_params)
     end
-    
+
     def method_missing(method, *args)
       APICategory.new(method.to_s, @api_key, @timeout, @throws_exceptions, @default_params)
     end
