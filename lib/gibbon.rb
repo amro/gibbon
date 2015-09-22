3dcf7bdb3c90b192007cdb9743e0d8cb920d4340
Amro Mousa <amromousa@gmail.com>
2015-09-22 15:02:46 -0400

    Add Export API support

--- lib/gibbon.rb
+++ lib/gibbon.rb
@@ -1,11 +1,14 @@
 require 'faraday'
 require 'multi_json'
 require 'cgi'
+require 'uri'
+require 'net/http'
 
 require 'gibbon/gibbon_error'
 require 'gibbon/mailchimp_error'
 require 'gibbon/request'
 require 'gibbon/api_request'
+require 'gibbon/export_request'
 
 module Gibbon
 end
