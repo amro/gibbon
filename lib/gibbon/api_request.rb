2bd9a294a5ad4c94477a749d6e74fb875a646000
Darren Cheng <darren@thanx.com>
2019-10-01 19:24:10 -0700

    Add support for Faraday v0.16.0 error class changes.

--- lib/gibbon/api_request.rb
+++ lib/gibbon/api_request.rb
@@ -109,7 +109,10 @@ module Gibbon
       error_params = {}
 
       begin
-        if error.is_a?(Faraday::Error::ClientError) && error.response
+        # Faraday::ClientError is used in Faraday 0.16.0+
+        # Faraday::Error::ClientError was used before 0.16.0
+        client_error = error.is_a?(Faraday::ClientError) || error.is_a?(Faraday::Error::ClientError)
+        if client_error && error.response
           error_params[:status_code] = error.response[:status]
           error_params[:raw_body] = error.response[:body]
 
